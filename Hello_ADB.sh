e06df03f2b1067a5d0393b020047b4f5f22f01f6
Teslenko <Teslvova@gmail.com>
2017-06-29 15:38:45 +0300

    ADB command

---
+++ Hello_ADB.sh
@@ -0,0 +1,24 @@
+adb shell input tap 132 781 		#click on GoogleChrome
+adb shell input tap 700 105 		#on "menu" butoon
+adb shell input tap 350 206 		#New Tab 
+adb shell input tap 0 0
+adb shell input tap 700 105 		#on "menu" butoon
+adb shell input tap 310 385 		#on Закладки
+adb shell input swipe 310 310 310 310 	# Click on Google.com 
+adb shell input tap 0 0
+adb shell input tap 310 600 		# Click on Search field
+adb shell input tap 0 0
+adb shell input tap 445 1050 		#H
+adb shell input tap 202 960 		#e
+adb shell input tap 665 1050 		#l
+adb shell input tap 665 1050 		#l
+adb shell input tap 620 952 		#0
+adb shell input tap 360 1241 		#_
+adb shell input tap 80 1142		#|
+adb shell input tap 580 1046 		#K
+adb shell input tap 553 951 		#i
+adb shell input tap 264 951 		#r
+adb shell input tap 553 951 		#i
+adb shell input tap 665 1050 		#l
+adb shell input tap 665 1050 		#l
+adb shell input tap 684 562 		#Search
